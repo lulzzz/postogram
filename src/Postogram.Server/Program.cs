9a179255544eedb39eda10cc84a6dab74b43473d
Евгений Стрелец <jkpro@yandex.ru>
2018-03-31 21:21:01 +0700

    added: Autofac

--- src/Postogram.Server/Program.cs
+++ src/Postogram.Server/Program.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Autofac;
 using Postogram.Common;
+using Postogram.Common.Configuration;
+using Postogram.Common.Logger;
 using Postogram.Server.Configuration;
 using Postogram.Server.Logger;
 
@@ -13,11 +16,34 @@ namespace Postogram.Server
     {
         static void Main(string[] args)
         {
-            var logger = new SerilogAdapter(new FileHelper());
-            var writer = logger.CreateWriter<Program>();
-            writer.Info("Hello '{World}'", new { World = "Mars" });
+            var builder = new ContainerBuilder();
+            builder.RegisterModule<ServerModule>();
+            var container = builder.Build(Autofac.Builder.ContainerBuildOptions.None);
+
+            var ex = container.Resolve<Example>();
 
             Console.ReadLine();
         }
+    }
+
+    public class Example
+    {
+        public Example(InstagramConfiguration section, ILogger logger)
+        {
+            var writer = logger.CreateWriter<Example>();
+            writer.Info("Login {login} password {password}", section.Login, section.Password);
+        }
+    }
+
+    public class InstagramConfiguration : IConfigurationSection
+    {
+        public void Init(IConfigurationReader reader)
+        {
+            Login = reader.Read<string>(nameof(Login));
+            Password = reader.Read<string>(nameof(Password));
+        }
+
+        public string Login { get; private set; }
+        public string Password { get; private set; }
     }
 }
---
+++ src/Postogram.Server/ServerModule.cs
@@ -0,0 +1,23 @@
+using Autofac;
+using Postogram.Common;
+using Postogram.Common.Configuration;
+using Postogram.Common.Logger;
+using Postogram.Server.Configuration;
+using Postogram.Server.Logger;
+
+namespace Postogram.Server
+{
+    public class ServerModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterInstance(new FileHelper()).As<FileHelper>();
+            builder.RegisterType<SerilogAdapter>().As<ILogger>();
+
+            builder.RegisterType<AppSettingsConfiguration>().As<IConfiguration>();
+            builder.RegisterConfigurationSection<InstagramConfiguration>();
+            builder.RegisterType<Example>();
+        }
+        
+    }
+}
