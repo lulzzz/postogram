10e45889687092d7096ee6511cd688a1e62dd14c
Strelets <e.str.94@yandex.ru>
2018-09-26 14:52:35 +0700

    Profiles model

---
+++ src/Postogram.InstagramClient/InstagramProfile.cs
@@ -0,0 +1,10 @@
+using Postogram.Common;
+
+namespace Postogram.InstagramClient
+{
+    public class InstagramProfile : Profile
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
